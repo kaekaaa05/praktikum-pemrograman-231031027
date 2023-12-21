41abf01ab6a4bde43ad88be01d14a8abf74c032d
kaekaaa05 <153788340+kaekaaa05@users.noreply.github.com>
2023-12-21 10:06:56 +0800

    upload latihan 1

---
+++ 231031027-eka-latihan1.py
@@ -0,0 +1,11 @@
+print('##### LATIHAN 1 #####')
+
+nilai = 70
+nilai = float(input('Masukkan Nilai(0-100): '))
+batas_lulus = 65
+# nl = input('Masukkan nilai (0-100): ')
+
+if nilai > batas_lulus :
+    print('Selamat, kamu lulus!')
+else:
+    print('Sayang sekali, kamu tidak lulus!')
