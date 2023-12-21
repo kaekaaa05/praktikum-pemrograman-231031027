f74c31eb64d90d440fbc697689091c9dd310cbc1
kaekaaa05 <153788340+kaekaaa05@users.noreply.github.com>
2023-12-21 10:07:40 +0800

    upload latihan 3

---
+++ 231031027-eka-latihan3.py
@@ -0,0 +1,10 @@
+print('##### LATIHAN 3 #####')
+
+nama = input('Masukkan Nama Karyawan : ')
+pendapatan = int(input('Pendapatan : '))
+
+if pendapatan > 1000 :
+    print('Status : Berhak')
+
+else:
+    print('Status : Tidak Berhak')
