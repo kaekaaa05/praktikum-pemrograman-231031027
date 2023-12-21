9982448dd41b52a4a6a903568be9500de48967fc
kaekaaa05 <153788340+kaekaaa05@users.noreply.github.com>
2023-12-21 10:01:12 +0800

    upload tugas a1 operator

---
+++ tugas.py
@@ -0,0 +1,33 @@
+print('##Operasi Aritmatika##')
+print()
+a = 32
+b = 4
+print('##Operasi Penjumlahan##')
+hasil = a + b
+print(a,'+',b,'=',hasil)
+print()
+print('##Operasi Pengurangan##')
+hasil = a - b
+print(a,'-',b,'=',hasil)
+print()
+print('##Operasi Perkalian##')
+hasil = a * b
+print(a,'*',b,'=',hasil)
+print()
+print('##Operasi Pembagian##')
+hasil = a / b
+print(a,'/',b,'=',hasil)
+print()
+print('##Operasi Eksponensial##')
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+print()
+print('##Operasi Modulus##')
+hasil = a % b
+print(a,'%',b,'=',hasil)
+print()
+print('##Operasi Floor Division##')
+hasil = a // b
+print(a,'//',b,'=',hasil)
+
+
