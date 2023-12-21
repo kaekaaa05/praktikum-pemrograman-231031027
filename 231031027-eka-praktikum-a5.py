96e1ec8e3696f0974860ea44659060347118b8cb
kaekaaa05 <153788340+kaekaaa05@users.noreply.github.com>
2023-12-21 09:53:59 +0800

    upload praktikum a5

---
+++ 231031027-eka-praktikum-a5.py
@@ -0,0 +1,10 @@
+a = True
+# input angka 1 ke variabel "perulangan"
+perulangan = 1
+
+# variabel kurang sama dengan 5,
+while perulangan <= 5:
+    print('Menjalankan Program')
+# menaikkan nilai variabel "perulangan" sebanyak 1 angka dalam setiap perulangan
+    perulangan += 1
+
